4783bfe372fff5ba1c72b63d981fc3a03b75df3d
gavin <79165709+mynameisgav1n@users.noreply.github.com>
2025-04-27 16:57:03 -0400

    Update index.tsx

--- packages/editor-sample/src/App/SamplesDrawer/index.tsx
+++ packages/editor-sample/src/App/SamplesDrawer/index.tsx
@@ -52,26 +52,22 @@ export default function SamplesDrawer() {
           </Stack>
         </Stack>
         <Stack spacing={2} px={0.75} py={3}>
-          <Link href="https://usewaypoint.com?utm_source=emailbuilderjs" target="_blank" sx={{ lineHeight: 1 }}>
-            <Box component="img" src={logo} width={32} />
-          </Link>
           <Box>
             <Typography variant="overline" gutterBottom>
-              Looking to send emails?
+              Need help?
             </Typography>
             <Typography variant="body2" color="text.secondary" paragraph>
-              Waypoint is an end-to-end email API with a &apos;pro&apos; version of this template builder with dynamic
-              variables, loops, conditionals, drag and drop, layouts, and more.
+              Contact Gavin at gavin@inspireyouthnj.org.
             </Typography>
           </Box>
           <Button
             variant="contained"
             color="primary"
             sx={{ justifyContent: 'center' }}
-            href="https://usewaypoint.com?utm_source=emailbuilderjs"
+            href="mailto:gavin@inspireyouthnj.org"
             target="_blank"
           >
-            Learn more
+            Send email
           </Button>
         </Stack>
       </Stack>
