3f0c0ba9d4e2f2bc55d1dbb0348be6b109c010f4
gavin <79165709+mynameisgav1n@users.noreply.github.com>
2025-04-29 12:39:54 -0400

    Update index.tsx

--- packages/editor-sample/src/App/TemplatePanel/index.tsx
+++ packages/editor-sample/src/App/TemplatePanel/index.tsx
@@ -112,7 +112,6 @@ export default function TemplatePanel() {
                 </Tooltip>
               </ToggleButton>
             </ToggleButtonGroup>
-            <ShareButton />
             <ShortenButton />
             <SaveButton />
             <SendButton />
